ec22f7a1c73a4b5e2bc1aab474e03f6535758e04
Ryan Hueckel <jrhuecke@ucsc.edu>
2022-10-19 12:48:27 -0700

    Fixed windows facing wrong way

--- Assets/Scripts/CameraSwapper.cs
+++ Assets/Scripts/CameraSwapper.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UIElements;
+    
 public class CameraSwapper : MonoBehaviour
 {
     public GameObject firstPerson;
@@ -10,6 +11,8 @@ public class CameraSwapper : MonoBehaviour
     public GameObject scriptedView;
 
     private int current;
+    private VisualElement frame;
+    private Button button;
 
     private void Start()
     {
@@ -20,36 +23,49 @@ public class CameraSwapper : MonoBehaviour
         current = 3;
     }
 
-    void Update()
+    private void OnEnable()
     {
-        //Swaps between cameras on button press 1-4
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        //Get the button which is nested in the UI Document
+        var uiDocument = GetComponent<UIDocument>();
+        var rootVisualElement = uiDocument.rootVisualElement;
+        frame = rootVisualElement.Q<VisualElement>("Frame");
+        button = frame.Q<Button>("CameraSwapper");
+        button.RegisterCallback<ClickEvent>(ev => ChangeCamera());
+    }
+
+    private void ChangeCamera()
+    {
+        current++;
+        if (current > 4)
         {
-            firstPerson.SetActive(true);
-            thirdPerson.SetActive(false);
-            sceneView.SetActive(false);
-            scriptedView.SetActive(false);
-        } 
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            firstPerson.SetActive(false);
-            thirdPerson.SetActive(true);
-            sceneView.SetActive(false);
-            scriptedView.SetActive(false);
+            current = 1;
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        switch (current)
         {
-            firstPerson.SetActive(false);
-            thirdPerson.SetActive(false);
-            sceneView.SetActive(true);
-            scriptedView.SetActive(false);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            firstPerson.SetActive(false);
-            thirdPerson.SetActive(false);
-            sceneView.SetActive(false);
-            scriptedView.SetActive(true);
+            case 1:
+                firstPerson.SetActive(true);
+                thirdPerson.SetActive(false);
+                sceneView.SetActive(false);
+                scriptedView.SetActive(false);
+                break;
+            case 2:
+                firstPerson.SetActive(false);
+                thirdPerson.SetActive(true);
+                sceneView.SetActive(false);
+                scriptedView.SetActive(false);
+                break;
+            case 3:
+                firstPerson.SetActive(false);
+                thirdPerson.SetActive(false);
+                sceneView.SetActive(true);
+                scriptedView.SetActive(false);
+                break;
+            case 4:
+                firstPerson.SetActive(false);
+                thirdPerson.SetActive(false);
+                sceneView.SetActive(false);
+                scriptedView.SetActive(true);
+                break;
         }
     }
 }
